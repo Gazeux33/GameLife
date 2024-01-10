a1cb00654d60c6bd2a760bc2b4c3cf37645e949e
Theo <thoecastillo@yahoo.com>
2024-01-10 15:02:21 +0100

    up

--- gameLife.js
+++ gameLife.js
@@ -1,13 +1,59 @@
-const bgColor = 0;
-const lineColor = 0;
-const cellColor = "#D651E8";
+const submitButton = document.getElementById('update')
+const inputSize = document.getElementById('size')
 
-const matriceSize = 200;
-const cellSize = 4;
+const inputFps = document.getElementById('fps')
+const fpsDisplay = document.getElementById('fpsValue')
+
+const inputColorBg = document.getElementById('background')
+
+const inputColorCell = document.getElementById('cell')
+
+const inputColorLine = document.getElementById('line')
+const sizeValueDisplay = document.getElementById('sizeValue');
+
+const screenSize = window.innerHeight*0.7;
+console.log(screenSize);
+
+let bgColor;
+let cellColor;
+let matriceSize;
+let cellSize;
+let fps;
 const matrice = [];
 const newMatrice = [];
-
 const startCell = [];
+
+
+inputSize.addEventListener('change', updateSizeDisplay);
+inputFps.addEventListener('change', updateFpsDisplay);
+
+submitButton.addEventListener('click',(e)=>{
+  getVar()
+  setup()
+})
+
+function updateSizeDisplay() {
+  sizeValueDisplay.textContent = inputSize.value;
+}
+
+function updateFpsDisplay() {
+  
+  fpsValue.textContent = inputFps.value;
+}
+
+function getVar(){
+  fps = inputFps.value
+  bgColor = inputColorBg.value
+  cellColor = inputColorCell.value
+  matriceSize = inputSize.value
+  cellSize =  screenSize/matriceSize
+}
+
+
+
+getVar()
+updateSizeDisplay();
+
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -81,34 +127,28 @@ function showMatrice() {
     for (let i = 0; i < tab.length; i++) {
       row += tab[i] + "";
     }
-    console.log(row);
   }
 }
 
 function setup() {
-  createCanvas(matriceSize * cellSize, matriceSize * cellSize);
+  console.log("change to"+fps)
+  frameRate(fps)
+  createCanvas(screenSize,screenSize);
   background(bgColor);
   createMatrice();
   setStartMatrice();
   fillRandomMatrice();
   showMatrice();
   stroke(0);
+
   
 }
 
 function draw() {
+  console.log(fps)
   background(bgColor);
-  drawGrid();
   fillCell();
   checkDeadCell();
-}
-
-function drawGrid() {
-  stroke(lineColor);
-  for (let i = cellSize; i <= matriceSize * cellSize; i += cellSize) {
-    line(i, 0, i, height);
-    line(0, i, width, i);
-  }
 }
 
 function fillCell() {
